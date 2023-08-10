5e86698d9a2e71f58256f4465ca807a62d14d280
InnaUsenko <innausenko8@gmail.com>
2023-08-10 14:30:00 +0000

    load a kitty

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,8 +3,22 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_CttqUDgsl3QpcnrJnabHDIavdhXusNOK6meoDtBuGton0oococU1QacoNfuXCiAb';
 
-export function FetchBreeds() {
+export function fetchBreeds() {
   return fetch('https://api.thecatapi.com/v1/breeds').then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  const searchParams = new URLSearchParams({
+    breed_ids: breedId,
+  });
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?${searchParams}`
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,47 @@
 const catSelector = document.querySelector('.breed-select');
 const catLoader = document.querySelector('.loader');
-import { FetchBreeds } from './cat-api.js';
+const catError = document.querySelector('.error');
+import { fetchBreeds } from './cat-api.js';
+import { fetchCatByBreed } from './cat-api.js';
 
-let cats = new FetchBreeds();
-cats
+let catsPromice = new fetchBreeds();
+catsPromice
   .then(kitty => {
-    catSelector.innerHTML = kitty
-      .map(cat => {
-        return `<option value=${cat.id}>${cat.name}</option>`;
-      })
-      .join('');
+    catSelector.innerHTML = FillSelector(kitty);
   })
-  .catch(error => console.log(error));
+  .catch(error => {
+    console.log(error);
+    catError.innerHTML('ERROR: ' + error);
+  });
 
-catSelector.addEventListener('click', () => {
-  let options = catSelector.querySelectorAll('option');
-  catLoader.innerHTML = `Cats found: ${options.length}`;
+catSelector.addEventListener('change', event => {
+  //let options = catSelector.querySelectorAll('option');
+  const option = event.currentTarget.value;
+  const catPromice = fetchCatByBreed(option);
+  const pict = catPromice
+    .then(kitty => {
+      if (kitty.length > 0) {
+        catLoader.innerHTML = CreatePictures(kitty);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      catError.innerHTML('ERROR: ' + error);
+    });
 });
+
+function FillSelector(objList) {
+  return objList
+    .map(obj => {
+      return `<option value=${obj.id}>${obj.name}</option>`;
+    })
+    .join('');
+}
+
+function CreatePictures(objList) {
+  return objList
+    .map(obj => {
+      return `<img src=${obj.url} alt=${obj.id} width=320px/>`;
+    })
+    .join('');
+}
