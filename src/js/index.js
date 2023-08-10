4912d6e31c0fbc72f5a34070894d03a63c5122d1
InnaUsenko <innausenko8@gmail.com>
2023-08-10 17:04:51 +0000

    cat info

--- src/js/index.js
+++ src/js/index.js
@@ -4,24 +4,36 @@ const catError = document.querySelector('.error');
 import { fetchBreeds } from './cat-api.js';
 import { fetchCatByBreed } from './cat-api.js';
 
+let cats = [];
+
 let catsPromice = new fetchBreeds();
 catsPromice
   .then(kitty => {
     catSelector.innerHTML = FillSelector(kitty);
+    cats = kitty;
   })
   .catch(error => {
     console.log(error);
-    catError.innerHTML('ERROR: ' + error);
+    catError.innerHTML = 'ERROR: ' + error;
   });
 
 catSelector.addEventListener('change', event => {
   //let options = catSelector.querySelectorAll('option');
   const option = event.currentTarget.value;
   const catPromice = fetchCatByBreed(option);
+  const catInfo = cats.find(cat => cat.id === option);
+  console.log('cats: ' + cats.length);
   const pict = catPromice
     .then(kitty => {
       if (kitty.length > 0) {
-        catLoader.innerHTML = CreatePictures(kitty);
+        catLoader.insertAdjacentHTML('afterbegin', CreatePictures(kitty));
+        console.log(kitty);
+        if (catInfo) {
+          catLoader.insertAdjacentHTML(
+            'beforeend',
+            `<h2>${catInfo.name}</h2><p>${catInfo.description}</p><p><b>Temperament:</b>${catInfo.temperament}</p>`
+          );
+        }
       }
     })
     .catch(error => {
